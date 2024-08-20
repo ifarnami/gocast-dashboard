4b9e2fb15cffc68066431ef5622383189011aa36
ifarnami <farnam.faryar@gmail.com>
2024-08-20 18:33:52 +0330

    feat: add theme provider

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,24 @@
+import { useMemo, useState } from "react";
+import { createTheme, PaletteMode } from "@mui/material";
+import { getDesignTokens } from "../config/mui";
+
+const useTheme = () => {
+  const [mode, setMode] = useState<PaletteMode>("light");
+
+  const toggleThemeMode = () => {
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
+
+  const modifiedTheme = useMemo(
+    () => createTheme(getDesignTokens(mode)),
+    [mode]
+  );
+
+  return {
+    theme: modifiedTheme,
+    mode,
+    toggleThemeMode,
+  };
+};
+
+export default useTheme;
---
+++ src/provider/ThemeColorProvider.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren } from "react";
+import { themeContext } from "./../context/ThemeContext";
+import useTheme from "../hooks/useTheme";
+import { ThemeProvider } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+
+const ThemeColorProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const value = useTheme();
+
+  return (
+    <themeContext.Provider value={value}>
+      <ThemeProvider theme={value.theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </themeContext.Provider>
+  );
+};
+
+export default ThemeColorProvider;
